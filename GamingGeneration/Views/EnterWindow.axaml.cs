a9b653748f7fa33f4a17225b5b6143bc385227c3
Wastopous <wastopous23@mail.ru>
2024-03-06 02:04:51 +0700

    2

---
+++ GamingGeneration/Views/EnterWindow.axaml.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using GamingGeneration.Views;
+
+namespace GamingGeneration;
+
+public partial class EnterWindow : Window
+{
+    public EnterWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void EnterButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var mainWindow = new MainWindow();
+        this.Hide();
+        mainWindow.Show();
+    }
+}
--- GamingGeneration/Views/MainWindow.axaml.cs
+++ GamingGeneration/Views/MainWindow.axaml.cs
@@ -1,14 +1,64 @@
+using System;
+using System.Text;
+using System.Threading.Tasks;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Media;
 
 namespace GamingGeneration.Views;
 
 public partial class MainWindow : Window
 {
+
     public MainWindow()
     {
         InitializeComponent();
+        this.Closing += MainWindow_Closing;
+        PassGenButton = this.FindControl<Button>("PassGenButton");
+        PassTextBox = this.FindControl<TextBox>("PassTextBox");
+    }
+    private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+    {
+
+        var enterWindow = new EnterWindow();
+        enterWindow.Show();
     }
 
 
+    private async void PassGenButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        PassGenButton.Background = new SolidColorBrush(Color.Parse("#3083df"));
+        PassGenButton.Content = "ГЕНЕРАЦИЯ";
+        await Task.Delay(TimeSpan.FromSeconds(1));
+        string passGen = GeneratePassword();
+        PassTextBox.Text = passGen;
+        PassGenButton.Content = "\u25b6  СГЕНЕРИРОВАТЬ ПАРОЛЬ";
+        PassGenButton.Background = new SolidColorBrush(Color.Parse("#6dd51f"));
+    }
+
+    private string GeneratePassword()
+    {
+        const string upChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const string lowChars = "abcdefghijklmnopqrstuvwxyz";
+        const string dgChars = "0123456789";
+        const string specChars = "!@#$%&*_?";
+
+        StringBuilder pass = new StringBuilder();
+        Random r = new Random();
+
+        
+        for (int i = 0; i < 8; i++)
+        {
+            pass.Append(lowChars[r.Next(lowChars.Length)]);
+        }
+        pass.Append(upChars[r.Next(upChars.Length)]);
+        
+        for (int i = 0; i < 3; i++)
+        {
+            pass.Append(dgChars[r.Next(dgChars.Length)]);
+        }
+        pass.Append(specChars[r.Next(specChars.Length)]);
+
+        return pass.ToString();
+    }
 }
